10f60211ce7efe299b62e10f631f17fdbc021d3f
luthfi_puji1 <luthfi_puji1@student.uns.ac.id>
2021-03-17 21:26:26 +0700

    installasi, konfigurasi router dan create api

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
+//import bootstrap, popper.js dan jquery
+import 'bootstrap/dist/css/bootstrap.css'
+import 'jquery/dist/jquery.min'
+import 'popper.js/dist/popper.min'
+import 'bootstrap/dist/js/bootstrap.min'
+
+createApp(App).mount('#app')
